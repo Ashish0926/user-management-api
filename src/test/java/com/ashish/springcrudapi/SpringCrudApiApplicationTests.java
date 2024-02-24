486208c789c151be71d5872c52f3b06149aecc95
Ashish Gupta <ashish.gupta@global.com>
2024-02-24 09:20:45 +0530

    first commit

---
+++ src/test/java/com/ashish/springcrudapi/SpringCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.springcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
